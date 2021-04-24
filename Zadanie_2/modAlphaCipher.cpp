860f55b47c307a3bdb2dc35dac59fc4429f089fe
dvadcat <83099623+dvadcat@users.noreply.github.com>
2021-04-24 12:16:12 +0300

    Update modAlphaCipher.cpp

--- Zadanie_2/modAlphaCipher.cpp
+++ Zadanie_2/modAlphaCipher.cpp
@@ -1,14 +1,14 @@
 #include "modAlphaCipher.h"
 
-string modAlphaCipher::decoder(const std::string& cipher_st) // РАСШИФРОВАНИЕ
+string modAlphaCipher::decoder(const std::string& cipher_st) 
 {
     string n_s = cipher_st;
-    int dlina, nstrok, position, a; // dlina-длина строки ; nstrok-количество строк ; position-новая позиция символа, который меняем ;
-    dlina = cipher_st.size(); // st-строка с текстом, который вводится пользователем
-    nstrok = (dlina - 1) / newkey + 1; // newkey-количество столбцов в таблице
+    int dlina, nstrok, position, a; 
+    dlina = cipher_st.size(); 
+    nstrok = (dlina - 1) / newkey + 1; 
     a = 0;
-    for (int colum_number = newkey; colum_number > 0; colum_number--) { // colum_number-номер столбца
-        for (int line_number = 0; line_number < nstrok; line_number++) { // line_number-номер строки
+    for (int colum_number = newkey; colum_number > 0; colum_number--) { 
+        for (int line_number = 0; line_number < nstrok; line_number++) { 
             position = newkey*line_number+colum_number; //
             if (position-1 < dlina) {
                 n_s[position-1] = cipher_st[a];
@@ -18,15 +18,15 @@ string modAlphaCipher::decoder(const std::string& cipher_st) // РАСШИФРО
     }
     return n_s;
 }
-string modAlphaCipher::coder(const string& open_st) // ЗАШИФРОВАНИЕ
+string modAlphaCipher::coder(const string& open_st) 
 {
     string n_s = open_st;
-    int dlina, nstrok, position, a; // dlina-длина строки ; nstrok-количество строк ; position-новая позиция символа, который меняем ;
-    dlina = open_st.size(); // st-строка с текстом, который вводится пользователем
-    nstrok = (dlina - 1) / newkey + 1; //newkey-количество столбцов в таблице
+    int dlina, nstrok, position, a; 
+    dlina = open_st.size(); 
+    nstrok = (dlina - 1) / newkey + 1;
     a = 0;
-    for (int colum_number = newkey; colum_number > 0; colum_number--) { // colum_number-номер столбца
-        for (int line_number = 0; line_number < nstrok; line_number++) { // line_number-номер строки
+    for (int colum_number = newkey; colum_number > 0; colum_number--) { 
+        for (int line_number = 0; line_number < nstrok; line_number++) { 
             position = colum_number+newkey*line_number;
             if (position-1 < dlina) {
                 n_s[a] = open_st[position-1];
